0b6924517eaff553214010ff3aae519e0d072c40
selvendiranj <selvendiranj@gmail.com>
2016-09-23 16:55:12 +0530

    adding batch file for restore

--- 1.Restore.bat
+++ 1.Restore.bat
@@ -1,13 +1,13 @@
 
-dotnet restore "src/Infrastructure/PilotKit.Infrastructure.Interfaces"
-dotnet restore "src/Infrastructure/PilotKit.Infrastructure.CrossCutting"
-dotnet restore "src/Domain/PilotKit.Domain.Model"
-dotnet restore "src/Repository/PilotKit.Repository.Interfaces"
-dotnet restore "src/Domain/PilotKit.Domain.Interfaces"
-dotnet restore "src/Orchestration/PilotKit.Orchestration.Interfaces"
-dotnet restore "src/Repository/PilotKit.Repository.DataAccess"
-dotnet restore "src/Domain/PilotKit.Domain.Services"
-dotnet restore "src/Orchestration/PilotKit.Orchestration.Concrete"
-dotnet restore "src/GUI/PilotKit.GUI.Web"
+dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.Interfaces
+dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.CrossCutting
+dotnet restore src/5.Domain/PilotKit.Domain.Model
+dotnet restore src/4.Repository/PilotKit.Repository.Interfaces
+dotnet restore src/5.Domain/PilotKit.Domain.Interfaces
+dotnet restore src/6.Orchestration/PilotKit.Orchestration.Interfaces
+dotnet restore src/4.Repository/PilotKit.Repository.DataAccess
+dotnet restore src/5.Domain/PilotKit.Domain.Services
+dotnet restore src/6.Orchestration/PilotKit.Orchestration.Concrete
+dotnet restore src/7.GUI/PilotKit.GUI.Web
 
 pause
