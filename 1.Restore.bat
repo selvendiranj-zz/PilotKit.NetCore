153682549cb7ee999a7291f560f31ed8884e83ed
selvendiranj <selvendiranj@gmail.com>
2016-09-23 23:55:50 +0530

    Changing the restore and build order

--- 1.Restore.bat
+++ 1.Restore.bat
@@ -3,11 +3,21 @@ dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.Interfaces
 dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.CrossCutting
 dotnet restore src/5.Domain/PilotKit.Domain.Model
 dotnet restore src/4.Repository/PilotKit.Repository.Interfaces
-dotnet restore src/5.Domain/PilotKit.Domain.Interfaces
-dotnet restore src/6.Orchestration/PilotKit.Orchestration.Interfaces
 dotnet restore src/4.Repository/PilotKit.Repository.DataAccess
+dotnet restore src/5.Domain/PilotKit.Domain.Interfaces
 dotnet restore src/5.Domain/PilotKit.Domain.Services
+dotnet restore src/6.Orchestration/PilotKit.Orchestration.Interfaces
 dotnet restore src/6.Orchestration/PilotKit.Orchestration.Concrete
 dotnet restore src/7.GUI/PilotKit.GUI.Web
 
-pause
+
+dotnet build src/8.Infrastructure/PilotKit.Infrastructure.Interfaces
+dotnet build src/8.Infrastructure/PilotKit.Infrastructure.CrossCutting
+dotnet build src/5.Domain/PilotKit.Domain.Model
+dotnet build src/4.Repository/PilotKit.Repository.Interfaces
+dotnet build src/4.Repository/PilotKit.Repository.DataAccess
+dotnet build src/5.Domain/PilotKit.Domain.Interfaces
+dotnet build src/5.Domain/PilotKit.Domain.Services
+dotnet build src/6.Orchestration/PilotKit.Orchestration.Interfaces
+dotnet build src/6.Orchestration/PilotKit.Orchestration.Concrete
+dotnet build src/7.GUI/PilotKit.GUI.Web
