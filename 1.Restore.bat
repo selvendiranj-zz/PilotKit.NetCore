6bc70f5e183036324e15fbbd8e93c0d24e09d1e0
Jayaraman <167416@CTS.COM>
2016-09-23 13:16:56 +0530

    Restore bat and global config corrections

---
+++ 1.Restore.bat
@@ -0,0 +1,13 @@
+
+dotnet restore "src/Infrastructure/PilotKit.Infrastructure.Interfaces"
+dotnet restore "src/Infrastructure/PilotKit.Infrastructure.CrossCutting"
+dotnet restore "src/Domain/PilotKit.Domain.Model"
+dotnet restore "src/Repository/PilotKit.Repository.Interfaces"
+dotnet restore "src/Domain/PilotKit.Domain.Interfaces"
+dotnet restore "src/Orchestration/PilotKit.Orchestration.Interfaces"
+dotnet restore "src/Repository/PilotKit.Repository.DataAccess"
+dotnet restore "src/Domain/PilotKit.Domain.Services"
+dotnet restore "src/Orchestration/PilotKit.Orchestration.Concrete"
+dotnet restore "src/GUI/PilotKit.GUI.Web"
+
+pause
