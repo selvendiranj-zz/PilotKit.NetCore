df819eeef203a129c223c569ca69f01d434e822d
Selvendiran Jayaraman <Selvendiran Jayaraman>
2016-09-25 15:04:01 +0530

    adding pack command

--- 1.Restore.bat
+++ 1.Restore.bat
@@ -1,6 +1,7 @@
 
 dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.Interfaces
 dotnet build   src/8.Infrastructure/PilotKit.Infrastructure.Interfaces
+REM dotnet pack    src/8.Infrastructure/PilotKit.Infrastructure.Interfaces -o src/bin
 dotnet restore src/8.Infrastructure/PilotKit.Infrastructure.CrossCutting
 dotnet build   src/8.Infrastructure/PilotKit.Infrastructure.CrossCutting
 dotnet restore src/5.Domain/PilotKit.Domain.Model
